be788995910fd70834a7701ec14b631b8a612ac4
Subroto Chanda Shuvo <subrotochandashuvo@gmail.com>
2026-06-15 14:57:51 +0600

    code divided from server and app file in src folder

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,79 @@
+import type { Request, Response } from "express";
+import { pool } from "../../db";
+import { userService } from "./user.service";
+
+
+const createUser = async (req: Request, res: Response) => {
+    // console.log(req.body);
+    // const { name, email, password, age } = req.body;
+
+    try {
+        const result = await userService.createUserIntoDB(req.body)
+
+        // console.log(result.rows[0]);
+
+        res.status(201).json({
+            success: true,
+            message: "User Created Successfully!",
+            data: result.rows[0],
+        });
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            error: error
+        });
+    }
+}
+
+const getAllUsers = async (req: Request, res: Response) => {
+    //   res.send('Hello user!!!!')
+    try {
+        const result = await userService.getAllUsersFromDB();
+        res.status(200).json({
+            "message": "Express Server",
+            "Author": "Subroto Chanda shuvo",
+        })
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            error: error
+        });
+    }
+
+}
+
+const getSingleUser = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const result = await userService.getSingleUserFromDB(id as string);
+
+        if(result.rows.length === 0) { 
+            return res.status(500).json({
+                success: false,
+                message: "User not found!",
+                data : {}
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "User retrieved successfully!",
+            data: result.rows[0],
+
+        });
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            error: error
+        });
+    }
+}
+
+export const userController = {
+    createUser,
+    getAllUsers,
+    getSingleUser
+}
---
+++ src/modules/user/user.route.ts
@@ -0,0 +1,14 @@
+import { Router, type Request, type Response } from "express";
+import { pool } from "../../db";
+import { userController } from "./user.controller";
+
+const router = Router()
+
+
+router.post('/', userController.createUser);
+
+router.get('/', userController.getAllUsers);
+
+router.get('/:id', userController.getSingleUser);
+
+export const userRoute = router;
